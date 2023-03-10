f9565deab9078b83718fbb460c455f3250f1056c
Raizer4 <nikitakraska1@gmail.com>
2023-03-10 20:52:07 +0300

    3 день подряд нужно затащить до пн

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
 $(function () {
+    $('.header__btn').on('click', function () {
+        $('.rightside-menu').removeClass('rightside-menu--close');
+    });
+});
 
-})
+$(function () {
+    $('.rightside-menu__close').on('click', function () {
+        $('.rightside-menu').addClass('rightside-menu--close');
+    });
+});
